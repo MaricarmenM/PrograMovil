7d2c0a344524b6cc5520ce9c590fd0add1f31755
MaricarmenM <61471455+MaricarmenM@users.noreply.github.com>
2021-10-15 01:59:04 -0500

    practica 3.1

--- lib/src/screens/tareas_screen.dart
+++ lib/src/screens/tareas_screen.dart
@@ -28,6 +28,16 @@ class _TareasScreenState extends State<TareasScreen> {
       appBar: AppBar(
          backgroundColor: ColorSettings.colorPrimary,
          title: Text('Tareas'),
+         actions: [
+           IconButton(
+             onPressed: (){
+               Navigator.pushNamed(context,'/tareasCompletas').whenComplete((){
+                 setState(() {});
+               });
+             }, 
+             icon: Icon(Icons.checklist_rounded),
+             )
+         ],
       ),
       body: FutureBuilder(
         future: _databaseHelper.getAllHW(),
@@ -36,7 +46,7 @@ class _TareasScreenState extends State<TareasScreen> {
             return Center(child: Text('Ocurrio un error en la peticion'),);
           }else{
             if (snapshot.connectionState == ConnectionState.done) {
-              return _listadoTareas(snapshot.data!);
+                return _listadoTareas(snapshot.data!);
             } else {
               return Center(child: CircularProgressIndicator(),);
             }   
@@ -51,14 +61,15 @@ class _TareasScreenState extends State<TareasScreen> {
         },   
         child: const Icon(Icons.add),
       ),
+      
     );
   }
   Widget _listadoTareas(List<TareasModel> tareas){
     return RefreshIndicator(
       onRefresh: (){
-        return Future.delayed(
-          Duration(seconds: 2),
-        (){setState(() { });}
+        return Future.delayed(Duration(seconds: 2),() {
+          setState(() { });
+          }
         );
       },
       child: ListView.builder(
@@ -75,17 +86,150 @@ class _TareasScreenState extends State<TareasScreen> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     IconButton(
-                      onPressed: (){
-                        Navigator.push(
-                          context, 
-                          MaterialPageRoute(
-                           builder:(context)=> AgregarTareaScreen(tarea: tarea,)
-                          )
-                        );
-                      },
-                      icon: Icon(Icons.edit),
-                      iconSize: 18, 
-                    ),
+                    icon: Icon(Icons.more_vert),
+                    onPressed: () {
+                      showDialog(
+                        context: context, 
+                        builder: (BuildContext context){
+                          return AlertDialog(
+                            contentPadding: EdgeInsets.all(0),
+                            content: SingleChildScrollView(
+                              child:Column(
+                                children: [
+                                  InkWell(
+                                    onTap: (){
+                                      Navigator.push(
+                                        context, 
+                                        MaterialPageRoute(
+                                            builder:(context)=> AgregarTareaScreen(tarea: tarea,)
+                                        )
+                                      );
+                                    },
+                                    child: Container(
+                                      padding: EdgeInsets.all(20),
+                                      decoration: BoxDecoration(
+                                        border: Border(bottom: BorderSide(width: 1,color: Colors.grey)) 
+                                      ),
+                                      child: Row(
+                                        children: [
+                                          Expanded(
+                                            child: Text(' Editar Tarea')
+                                            ),
+                                          Icon(Icons.edit_rounded),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                  InkWell(
+                                    onTap: (){
+                                      showDialog(
+                                          context: context,
+                                          builder: (context){
+                                            return AlertDialog(
+                                              title: Text('Confirmación'),
+                                              content: Text('Estas seguro de eliminar la tarea?'),
+                                              actions: [
+                                                TextButton(
+                                                  onPressed: (){
+                                                    Navigator.pop(context);
+                                                    _databaseHelper.deleteHW(tarea.id!).then(
+                                                      (noRows){
+                                                        if (noRows>0) {
+                                                          ScaffoldMessenger.of(context).showSnackBar(
+                                                          SnackBar(content: Text('Registro eliminado')));
+                                                          setState(() {});
+                                                        }
+                                                      }
+                                                    );
+                                                  },
+                                                  child: Text('si')
+                                                ),
+                                                TextButton(
+                                                  onPressed: (){
+                                                    Navigator.pop(context);
+                                                  }, 
+                                                  child: Text('no')
+                                                  )
+                                              ],
+                                            );
+                                          }
+                                      );
+                                    },
+                                    child: Container(
+                                      padding: EdgeInsets.all(20),
+                                      decoration: BoxDecoration(
+                                        border: Border(bottom: BorderSide(width: 1,color: Colors.grey)),
+                                      ),
+                                      child: Row(
+                                        children: [
+                                          Expanded(
+                                            child: Text('Eliminar Tarea'),
+                                            ),
+                                          Icon(Icons.delete_rounded)
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                  InkWell(
+                                    onTap: (){
+                                        tarea.entregada = 'SI';
+                                        showDialog(
+                                          context: context,
+                                          builder: (context) {
+                                            return AlertDialog(
+                                              title: Text('Confirmación'),
+                                              content: Text(
+                                                  'La tarea se marcara como entregada?'),
+                                              actions: [
+                                                TextButton(
+                                                 onPressed: () {
+                                                      Navigator.pop(context);
+                                                      _databaseHelper
+                                                          .updateHW(tarea.toMap())
+                                                          .then((noRows) {
+                                                        if (noRows > 0) {
+                                                          ScaffoldMessenger.of(context).showSnackBar(
+                                                            SnackBar(
+                                                                  content: Text('Tarea entregada con exito!!'))
+                                                            );
+                                                          setState(() {});
+                                                        }
+                                                      });
+                                                    },
+                                                    child: Text('OK')),
+                                                TextButton(
+                                                    onPressed: () {
+                                                      Navigator.pop(context);
+                                                    },
+                                                    child: Text('Cancelar')),
+                                              ],
+                                            );
+                                          }).then((value) {
+                                        setState(() {});
+                                      });
+                                    },
+                                    child: Container(
+                                      padding: EdgeInsets.all(20),
+                                      decoration: BoxDecoration(
+                                        border: Border(bottom: BorderSide(width: 1,color: Colors.grey)),
+                                      ),
+                                      child: Row(
+                                        children: [
+                                          Expanded(
+                                            child: Text('Entregar Tarea'),
+                                            ),
+                                          Icon(Icons.check_circle_sharp),
+                                        ],
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ) ,
+                            ),
+                          );
+                        },
+                     );
+                    }),
                   ],
                 )
               ],
@@ -96,6 +240,8 @@ class _TareasScreenState extends State<TareasScreen> {
       ),
     );
   }
+
+
 }
 
 
